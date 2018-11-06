add8db3012e50a83ba36b8d6150ebb7327207823
liangdanming <liangdanming@rivamed.cn>
2018-11-06 17:37:09 +0800

    修改表头的高度和部分UI优化

--- myapplication/src/main/java/high/rivamed/myapplication/adapter/StockLeftAdapter.java
+++ myapplication/src/main/java/high/rivamed/myapplication/adapter/StockLeftAdapter.java
@@ -1,6 +1,6 @@
 package high.rivamed.myapplication.adapter;
 
-import android.support.v7.widget.CardView;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -28,7 +28,6 @@ public class StockLeftAdapter extends BaseQuickAdapter<SocketLeftTopBean.CstExpi
    private TextView mStock_card_number;
    private TextView mStock_card_outofdate;
    private TextView mStock_card_unexpired;
-   private CardView mStock_cardview;
    public StockLeftAdapter(int layout, List<SocketLeftTopBean.CstExpirationVosBean> data) {
       super(layout, data);
    }
@@ -42,18 +41,19 @@ public class StockLeftAdapter extends BaseQuickAdapter<SocketLeftTopBean.CstExpi
 	int number3 = item.getNearExpireCount();
 
 	mStock_card_number.setText(number1);
-      mStock_card_outofdate.setText("过期耗材：" + number2 +"个");
-      mStock_card_unexpired.setText("近效期耗材：" + number3+"个");
 
       if (number2>0){
+	   mStock_card_outofdate.setText("●  过期耗材：" + number2 +"个");
 	   mStock_card_outofdate.setTextColor(mContext.getResources().getColor(R.color.card_stock_text));
 	}else {
-	   mStock_card_outofdate.setTextColor(mContext.getResources().getColor(R.color.text_color_9));
+	   mStock_card_outofdate.setText(Html.fromHtml("<font color='#52C41A'>●&nbsp&nbsp</font>" +"<font color='#333333'>过期耗材：" +  number2 + "个</font>"));
 	}
 	if (number3>0){
+	   mStock_card_unexpired.setText("●  近效期耗材：" + number3+"个");
 	   mStock_card_unexpired.setTextColor(mContext.getResources().getColor(R.color.card_stock_text));
 	}else {
-	   mStock_card_unexpired.setTextColor(mContext.getResources().getColor(R.color.text_color_9));
+	   mStock_card_unexpired.setText(Html.fromHtml("<font color='#52C41A'>●&nbsp&nbsp</font>" +"<font color='#333333'>近效期耗材：" +  number3 + "个</font>"));
+
 	}
 //	mStock_cardview.setOnClickListener(new View.OnClickListener() {
 //	   @Override
@@ -68,7 +68,6 @@ public class StockLeftAdapter extends BaseQuickAdapter<SocketLeftTopBean.CstExpi
       mStock_card_number = ((TextView) helper.getView(R.id.stock_card_number));
       mStock_card_outofdate = ((TextView) helper.getView(R.id.stock_card_outofdate));
       mStock_card_unexpired = ((TextView) helper.getView(R.id.stock_card_unexpired));
-      mStock_cardview = ((CardView) helper.getView(R.id.stock_cardview));
 
 
    }
