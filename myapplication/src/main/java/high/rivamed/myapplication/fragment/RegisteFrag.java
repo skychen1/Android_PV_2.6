0a1d49a08392138703ec85afdcf83fc788dca8a9
liangdanming <liangdanming@rivamed.cn>
2019-01-10 14:22:57 +0800

    修复bug

--- myapplication/src/main/java/high/rivamed/myapplication/fragment/RegisteFrag.java
+++ myapplication/src/main/java/high/rivamed/myapplication/fragment/RegisteFrag.java
@@ -31,7 +31,7 @@ import high.rivamed.myapplication.bean.ThingDto;
 import high.rivamed.myapplication.dbmodel.BoxIdBean;
 import high.rivamed.myapplication.http.BaseResult;
 import high.rivamed.myapplication.http.NetRequest;
-import high.rivamed.myapplication.receiver.FrgNetWorkReceiver;
+import high.rivamed.myapplication.receiver.NetWorkReceiver;
 import high.rivamed.myapplication.utils.DialogUtils;
 import high.rivamed.myapplication.utils.EventBusUtils;
 import high.rivamed.myapplication.utils.LogUtils;
@@ -67,7 +67,7 @@ import static high.rivamed.myapplication.cont.Constants.THING_CODE;
  * 更新描述：   ${TODO}
  */
 
-public class RegisteFrag extends SimpleFragment implements FrgNetWorkReceiver.IntAction {
+public class RegisteFrag extends SimpleFragment implements NetWorkReceiver.IntAction {
 
    String TAG = "RegisteFrag";
    @BindView(R.id.frag_registe_name_edit)
@@ -104,7 +104,7 @@ public class RegisteFrag extends SimpleFragment implements FrgNetWorkReceiver.In
    private       List<DeviceNameBeanX.DeviceDictVos> mNameList;
    private       ThingDto                            mSnRecoverBean;
    public static List<ThingDto.DeviceVosBean> mDeviceVos = new ArrayList<>();//柜子list
-   public  FrgNetWorkReceiver netWorkReceiver;
+   public  NetWorkReceiver netWorkReceiver;
    private String             mBoxCode;
 
    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
@@ -130,7 +130,7 @@ public class RegisteFrag extends SimpleFragment implements FrgNetWorkReceiver.In
 
    private void applyNet() {
 	IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-	netWorkReceiver = new FrgNetWorkReceiver();
+	netWorkReceiver = new NetWorkReceiver();
 	mContext.registerReceiver(netWorkReceiver, filter);
 	netWorkReceiver.setInteractionListener(this);
 
