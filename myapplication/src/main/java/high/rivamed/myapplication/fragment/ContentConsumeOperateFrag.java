66458d6cb18fb0ebebc4a3439b2aa35d15f464d1
liangdanming <liangdanming@rivamed.cn>
2019-01-21 13:57:43 +0800

    增加无apk更新信息不能登录的问题,增加部分开柜门和关柜门的语音

--- myapplication/src/main/java/high/rivamed/myapplication/fragment/ContentConsumeOperateFrag.java
+++ myapplication/src/main/java/high/rivamed/myapplication/fragment/ContentConsumeOperateFrag.java
@@ -202,10 +202,12 @@ public class ContentConsumeOperateFrag extends BaseSimpleFragment {
 	LogUtils.i(TAG, "door   " + event.door);
 	mIsClick = event.isClick;
 	if (mIsClick) {
+	   MusicPlayer.getInstance().play(MusicPlayer.Type.DOOR_OPEN);
 	   mRgTopGone.setVisibility(View.VISIBLE);
 	   mRgMiddleGone.setVisibility(View.VISIBLE);
 	   mRgDownGone.setVisibility(View.VISIBLE);
 	} else {
+	   MusicPlayer.getInstance().play(MusicPlayer.Type.DOOR_CLOSED);
 	   mRgTopGone.setVisibility(View.GONE);
 	   mRgMiddleGone.setVisibility(View.GONE);
 	   mRgDownGone.setVisibility(View.GONE);
